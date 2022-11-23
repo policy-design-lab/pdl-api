3e89cc426a595f95827f1b697016a01358577114
Yong Wook Kim <ywkim@illinois.edu>
2022-11-23 13:15:41 -0600

    all programs endpoint implemented

--- app/controllers/pdl.py
+++ app/controllers/pdl.py
@@ -2,6 +2,7 @@ import app.utils.jsonutils as jsonutils
 
 from app.models.summary import Summary
 from app.models.state import State
+from app.models.allprograms import AllProgram
 
 
 def search():
@@ -33,6 +34,48 @@ def states_search():
             "val": state.state_code,
             "id": state.state_name
         } for state in states]
+
+    # return {"count": len(results), "states": results}
+    return results
+
+
+# GET all the entries from states table
+def allprograms_search():
+    allprograms = AllProgram.query.all()
+    results = [
+        {
+            'State': prog.state,
+            'Crop Insurance 2018': prog.crop_ins_2018,
+            'Crop Insurance 2019': prog.crop_ins_2019,
+            'Crop Insurance 2020': prog.crop_ins_2020,
+            'Crop Insurance 2021': prog.crop_ins_2021,
+            'Crop Insurance 2022': prog.crop_ins_2022,
+            'Crop Insurance Total': prog.crop_ins_total,
+            'SNAP 2018': prog.snap_2018,
+            'SNAP 2019': prog.snap_2019,
+            'SNAP 2020': prog.snap_2020,
+            'SNAP 2021': prog.snap_2021,
+            'SNAP 2022': prog.snap_2022,
+            'SNAP Total': prog.snap_total,
+            'Title I 2018': prog.title_1_2018,
+            'Title I 2019': prog.title_1_2019,
+            'Title I 2020': prog.title_1_2020,
+            'Title I 2021': prog.title_1_2021,
+            'Title I 2022': prog.title_1_2022,
+            'Title I Total': prog.title_1_total,
+            'Title II 2018': prog.title_2_2018,
+            'Title II 2019': prog.title_2_2019,
+            'Title II 2020': prog.title_2_2020,
+            'Title II 2021': prog.title_2_2021,
+            'Title II 2022': prog.title_2_2022,
+            'Title II Total': prog.title_2_total,
+            '2018 All Programs Total': prog.all_programs_total_2018,
+            '2019 All Programs Total': prog.all_programs_total_2019,
+            '2020 All Programs Total': prog.all_programs_total_2020,
+            '2021 All Programs Total': prog.all_programs_total_2021,
+            '2022 All Programs Total': prog.all_programs_total_2022,
+            '18-22 All Programs Total': prog.all_programs_total_18_22
+        } for prog in allprograms]
 
     # return {"count": len(results), "states": results}
     return results
--- app/models/practices.py
+++
@@ -1,9 +0,0 @@
-class Practices:
-    def __init__(self):
-        self.practice_name = None
-
-    def set_practice_name(self, practice_name):
-        self.practice_name = practice_name
-
-    def get_practice_name(self):
-        return self.practice_name
