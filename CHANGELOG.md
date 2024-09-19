a5a7bda9870655bc98d1cf8ce757a88412c3ae92
YONG WOOK KIM <ywkim312@users.noreply.github.com>
2024-09-19 10:12:04 -0500

    saving purpose

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Changed
+- Crop Insurance endpoints to use data from the database. [#241](https://github.com/policy-design-lab/pdl-api/issues/241)
+
 ## [0.17.0] - 2024-08-28
 
 ## Added
--- app/models/payment.py
+++ app/models/payment.py
@@ -18,8 +18,18 @@ class Payment(db.Model):
     base_acres = db.Column(db.Float)
     practice_code = db.Column(db.String())
     practice_code_variant = db.Column(db.String())
+    premium_policy_count = db.Column(db.Integer)
+    liability_amount = db.Column(db.Integer)
+    premium_amount = db.Column(db.Integer)
+    premium_subsidy_amount = db.Column(db.Integer)
+    indemnity_amount = db.Column(db.Integer)
+    farmer_premium_amount = db.Column(db.Integer)
+    loss_ratio = db.Column(db.Float)
+    net_farmer_benefit_amount = db.Column(db.Integer)
 
-    def __init__(self, title_id, subtitle_id, program_id, sub_program_id, state_code, year, payment, recipient_count, base_acres):
+    def __init__(self, title_id, subtitle_id, program_id, sub_program_id, state_code, year, payment, recipient_count,
+                 base_acres, premium_policy_count, liability_amount, premium_amount, premium_subsidy_amount,
+                 indemnity_amount, farmer_premium_amount, loss_ratio, net_farmer_benefit_amount):
         self.title_id = title_id
         self.subtitle_id = subtitle_id
         self.program_id = program_id
@@ -29,10 +39,30 @@ class Payment(db.Model):
         self.payment = payment
         self.recipient_count = recipient_count
         self.base_acres = base_acres
+        self.premium_policy_count = premium_policy_count
+        self.liability_amount = liability_amount
+        self.premium_amount = premium_amount
+        self.premium_subsidy_amount = premium_subsidy_amount
+        self.indemnity_amount = indemnity_amount
+        self.farmer_premium_amount = farmer_premium_amount
+        self.loss_ratio = loss_ratio
+        self.net_farmer_benefit_amount = net_farmer_benefit_amount
 
     def __repr__(self):
-        return 'Payments(title_id=%s, subtitle_id=%s, program_id=%s, sub_program_id=%s, state_code=%s, year=%s, payment=%s, recipient_count=%s, base_acres=%s)' % (
-            self.title_id, self.subtitle_id, self.program_id, self.sub_program_id, self.state_code, self.year, self.payment, self.recipient_count, self.base_acres)
+        return 'Payments(title_id=%s, subtitle_id=%s, program_id=%s, sub_program_id=%s, state_code=%s, year=%s, ' \
+               'payment=%s, recipient_count=%s, base_acres=%s, premium_policy_count=%s, liability_amount=%s, ' \
+               'premium_amount =%s, premium_subsidy_amount=%s, indemnity_amount=%s, farmer_premium_amount=%s,' \
+               'loss_ratio=%s, %net_farmer_benefit_amount)' % (
+            self.title_id, self.subtitle_id, self.program_id, self.sub_program_id, self.state_code, self.year,
+            self.payment, self.recipient_count, self.base_acres, self.premium_policy_count, self.liability_amount,
+            self.premium_amount, self.premium_subsidy_amount, self.indemnity_amount, self.farmer_premium_amount,
+            self.loss_ratio, self.net_farmer_benefit_amount)
 
     def json(self):
-        return {'title_id': self.title_id, 'subtitle_id': self.subtitle_id, 'program_id': self.program_id, 'sub_program_id': self.sub_program_id, 'state_code': self.state_code, 'year': self.year, 'payment': self.payment, 'recipient_count': self.recipient_count, 'base_acres': self.base_acres}
+        return {'title_id': self.title_id, 'subtitle_id': self.subtitle_id, 'program_id': self.program_id,
+                'sub_program_id': self.sub_program_id, 'state_code': self.state_code, 'year': self.year,
+                'payment': self.payment, 'recipient_count': self.recipient_count, 'base_acres': self.base_acres,
+                'premium_policy_count': self.premium_policy_count, 'liability_amount': self.liability_amount,
+                'premium_amount': self.premium_amount, 'premium_subsidy_amount': self.premium_subsidy_amount,
+                'indemnity_amount': self.indemnity_amount, 'farmer_premium_amount': self.farmer_premium_amount,
+                'loss_ratio': self.loss_ratio, 'net_farmer_benefit_amount': self.net_farmer_benefit_amount}
