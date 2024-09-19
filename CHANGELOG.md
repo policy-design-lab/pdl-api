a5c60fad62cce07e7c59935db9f0e0cf605efe1a
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2024-09-19 10:33:39 -0500

    Update ACEP and RCPP endpoints to use database (#251)
    * Update endpoints to remove hardcoded program and subtitle IDs.
    
    * Add Sub Sub Program model.
    
    * Update models for CRP.
    
    * Update CRP state distribution endpoint and generate part of the required JSON.
    
    * Add code to process sub-sub programs.
    
    * Update CRP summary endpoint to use the database.
    
    * Update CHANGELOG.
    
    * Use Total CRP sub program to generate the total values and exclude that sub program in the JSON response.
    
    * Update summary endpoints to include total values at the top.
    
    * Update CHANGELOG.md
    
    * Update ACEP endpoints to use database queries.
    
    * Update ACEP and RCPP endpoints to use database.
    
    * Update CHANGELOG.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Changed
+- CRP endpoints to use data from the database. [#237](https://github.com/policy-design-lab/pdl-api/issues/237)
+- ACEP endpoints to use data from the database. [#239](https://github.com/policy-design-lab/pdl-api/issues/239)
+- RCPP endpoints to use data from the database. [#241](https://github.com/policy-design-lab/pdl-api/issues/241)
+
 ## [0.17.0] - 2024-08-28
 
-## Added
+### Added
 - CSP IRA endpoints including state distribution and summary. [#234](https://github.com/policy-design-lab/pdl-api/issues/234)
 
 ### Changed
@@ -14,10 +21,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [0.16.0] - 2024-08-16
 
-## Changed
+### Changed
 - EQIP and CSP endpoints to use data from the database. [#203](https://github.com/policy-design-lab/pdl-api/issues/203)
 
-## Fixed
+### Fixed
 - Error when calling GET states endpoint after recent change. [#228](https://github.com/policy-design-lab/pdl-api/issues/228)
 - Error due to the previous SNAP program ID being used. [#230](https://github.com/policy-design-lab/pdl-api/issues/230)
 - Missing attributes in CSP response statute objects. [#232](https://github.com/policy-design-lab/pdl-api/issues/232) 
--- app/models/payment.py
+++ app/models/payment.py
@@ -10,29 +10,38 @@ class Payment(db.Model):
     subtitle_id = db.Column(db.Integer)
     program_id = db.Column(db.Integer)
     sub_program_id = db.Column(db.Integer)
+    sub_sub_program_id = db.Column(db.Integer)
     practice_category_id = db.Column(db.Integer)
     state_code = db.Column(db.String())
     year = db.Column(db.Integer)
     payment = db.Column(db.Float)
     recipient_count = db.Column(db.Integer)
+    contract_count = db.Column(db.Integer)
     base_acres = db.Column(db.Float)
+    farm_count = db.Column(db.Integer)
     practice_code = db.Column(db.String())
     practice_code_variant = db.Column(db.String())
 
-    def __init__(self, title_id, subtitle_id, program_id, sub_program_id, state_code, year, payment, recipient_count, base_acres):
+    def __init__(self, title_id, subtitle_id, program_id, sub_program_id, sub_sub_program_id, state_code, year, payment, recipient_count, contract_count, base_acres, farm_count, practice_code, practice_code_variant):
         self.title_id = title_id
         self.subtitle_id = subtitle_id
         self.program_id = program_id
         self.sub_program_id = sub_program_id
+        self.sub_sub_program_id = sub_sub_program_id
         self.state_code = state_code
         self.year = year
         self.payment = payment
         self.recipient_count = recipient_count
+        self.contract_count = contract_count
         self.base_acres = base_acres
+        self.farm_count = farm_count
+        self.practice_code = practice_code
+        self.practice_code_variant = practice_code_variant
 
     def __repr__(self):
-        return 'Payments(title_id=%s, subtitle_id=%s, program_id=%s, sub_program_id=%s, state_code=%s, year=%s, payment=%s, recipient_count=%s, base_acres=%s)' % (
-            self.title_id, self.subtitle_id, self.program_id, self.sub_program_id, self.state_code, self.year, self.payment, self.recipient_count, self.base_acres)
+        return 'Payments(title_id=%s, subtitle_id=%s, program_id=%s, sub_program_id=%s, sub_sub_program_id=%s, state_code=%s, year=%s, payment=%s, recipient_count=%s, contract_count=%s base_acres=%s, farm_count=%s, practice_code=%s, practice_code_variant=%s)' % (
+            self.title_id, self.subtitle_id, self.program_id, self.sub_program_id, self.sub_sub_program_id, self.state_code, self.year, self.payment, self.recipient_count, self.contract_count, self.base_acres, self
+            .farm_count, self.practice_code, self.practice_code_variant)
 
     def json(self):
-        return {'title_id': self.title_id, 'subtitle_id': self.subtitle_id, 'program_id': self.program_id, 'sub_program_id': self.sub_program_id, 'state_code': self.state_code, 'year': self.year, 'payment': self.payment, 'recipient_count': self.recipient_count, 'base_acres': self.base_acres}
+        return {'title_id': self.title_id, 'subtitle_id': self.subtitle_id, 'program_id': self.program_id, 'sub_program_id': self.sub_program_id, 'sub_sub_program_id': self.sub_sub_program_id, 'state_code': self.state_code, 'year': self.year, 'payment': self.payment, 'recipient_count': self.recipient_count, 'contract_count': self.contract_count, 'base_acres': self.base_acres, 'farm_count': self.farm_count, 'practice_code': self.practice_code, 'practice_code_variant': self.practice_code_variant}
