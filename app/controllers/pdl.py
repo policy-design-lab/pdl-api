c43dcf6ac2b36b9d799e1ca2de1a69e64418bdfc
Bing Zhang <bing@illinois.edu>
2026-06-24 10:21:14 -0500

    add operationId

--- app/controllers/pdl.py
+++ app/controllers/pdl.py
@@ -4093,6 +4093,5 @@ def generate_title_xi_summary_response(program_id, start_year, end_year):
 
     return aggregate_dict
 
-
 def titles_title_xi_crop_insurance_commodities():
     return {"commodities": cfg.COMMODITIES}
